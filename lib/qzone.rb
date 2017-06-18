65f83f22d08609441dfbf61aae6f683ffeeccf53
tuitu <1965972530@qq.com>
2017-06-18 23:10:18 +0800

    update code

--- lib/qzone.rb
+++ lib/qzone.rb
@@ -179,7 +179,7 @@ class Qzone
 
   def cookies_valid?
     begin
-      result = @spider.get "http://h5.qzone.qq.com/proxy/domain/tjalist.photo.qq.com/fcgi-bin/fcg_list_album_v3?g_tk=#{@gtk}&callback=shine0_Callback&t=419043014&hostUin=#{@user}&uin=#{@user}&appid=4&inCharset=utf-8&outCharset=utf-8&source=qzone&plat=qzone&format=jsonp&notice=0&filter=1&handset=4&pageNumModeSort=40&pageNumModeClass=15&needUserInfo=1&idcNum=5&callbackFun=shine0&_=1475649079634"
+      result = @spider.get(sprintf(Interface["ablum"], @gtk.to_s, @user, @user))
     rescue Exception => error
       if error.response_code == '403'
         raise 'Connection refused'
